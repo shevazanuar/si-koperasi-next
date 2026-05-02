fd76aee53715a700baf7e300b54c5d2b9c3e95e5
Sheva Zanuar <shevazanuar@gmail.com>
2026-05-02 08:08:50 +0700

    Remove sensitive scratch scripts

--- scratch/inspect_prisma.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-try {
-    const p = new PrismaClient();
-    console.log("Success with empty constructor");
-} catch (e) {
-    console.log("Error type:", e.constructor.name);
-    console.log("Error message:", e.message);
-}
